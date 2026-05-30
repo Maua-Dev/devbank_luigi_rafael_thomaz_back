c1cf2b0b9e9abe2a04f818daa7944363b226da52
unknown <thomazttp@gmail.com>
2026-05-30 12:33:46 -0300

    feat: main and test. missing only post withdraw

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,11 @@
+from hmac import new
+import stat
+
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
+
+from src.app.entities.transaction import Transaction
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
 
 from .environments import Environments
 
@@ -20,6 +26,60 @@ def get_history():
     transactions = user_repo.get_all_transactions()
     return{
         "all_transactions": [transaction.to_dict() for transaction in transactions]
+    }
+
+
+@app.post("/deposit")
+def post_deposit(request: dict):
+    for note, qty in request.items():
+        #check if the note somehow is not a number and if its valid
+        if not note.isdigit() or int(note) not in [2, 5, 10, 20, 50, 100, 200]:
+            raise HTTPException(status_code=400, detail="Nota inválida")
+        
+        #check if the note quantity somehow is not int
+        if type(qty) != int:
+            raise HTTPException(status_code=400, detail="Quantidade de notas deve ser um inteiro")
+        
+        #check if quantity is somehow less than 0
+        if qty < 0:
+            raise HTTPException(status_code=400, detail="Quantidade de notas não pode ser negativa")
+ 
+    notes = {
+        2: request.get("2", 0),
+        5: request.get("5", 0),
+        10: request.get("10", 0),
+        20: request.get("20", 0),
+        50: request.get("50", 0),
+        100: request.get("100", 0),
+        200: request.get("200", 0)
+    }
+    value = float(
+        sum(note * qty for note, qty in notes.items())
+    )
+
+    if value == 0.0:
+        raise HTTPException(status_code=400, detail="Sem notas selecionadas")
+    
+    if value >= 2 * user_repo.get_first_user().current_balance:
+        raise HTTPException(status_code=403, detail="Depósito suspeito")
+
+
+    current_balance = user_repo.get_first_user().current_balance
+    new_balance = current_balance + value
+
+    user_repo.update_balance(new_balance)
+
+    transaction = Transaction(
+            transaction_type=TransactionTypeEnum.DEPOSIT,
+            value=value,
+            current_balance=new_balance
+            #timestamp is generated after the constructor
+        )
+    user_repo.add_transaction(transaction)
+
+    return{
+        "current_balance": new_balance,
+        "timestamp": transaction.timestamp
     }
 
 
--- tests/app/test_main.py
+++ tests/app/test_main.py
@@ -1,22 +1,166 @@
-from fastapi.exceptions import HTTPException
 import pytest
+from fastapi.exceptions import HTTPException
+
 import src.app.main as main_module
-from src.app.main import get_user, get_history
-from src.app.repo.item_repository_mock import ItemRepositoryMock
+from src.app.main import get_user, get_history, post_deposit
 from src.app.repo.user_repository_mock import UserRepositoryMock
+from src.app.entities.transaction import Transaction
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
 
 
 class Test_Main:
-    
-    def test_get_user(self):
-        repo = UserRepositoryMock()
-        response = get_user()
-        print(response.get("name", None))
 
+    def setup_method(self):
+        main_module.user_repo = UserRepositoryMock()
+
+
+    def test_get_user(self):
+        response = get_user()
         assert response.get("name", None) == "Vitor Soller"
 
 
-    def test_get_history(self):
-        repo = UserRepositoryMock()
+    def test_get_history_empty(self):
         response = get_history()
+        assert response == {"all_transactions": []}
+
+    
+    def test_get_history_one_transaction(self):
+        repo = main_module.user_repo
+        #using the UserRepositoryMock() was throwing index error
+        response = get_history()
+        transaction = Transaction(
+            transaction_type = TransactionTypeEnum.DEPOSIT,
+            value = 100.0,
+            current_balance = 1100.0,
+            timestamp = 123456789.0
+        )
+        repo.add_transaction(transaction)
+
+        response = get_history()
+        assert response["all_transactions"][0] == transaction.to_dict()
+        assert len(response["all_transactions"]) == 1
+
+
+    def test_get_history_many_transactions(self):
+        repo = main_module.user_repo
+        #using the UserRepositoryMock() was throwing index error
+        response = get_history()
+        transaction1 = Transaction(
+            transaction_type = TransactionTypeEnum.DEPOSIT,
+            value = 100.0,
+            current_balance = 1100.0,
+            timestamp = 123456789.0
+        )
+        transaction2 = Transaction(
+            transaction_type = TransactionTypeEnum.WITHDRAW,
+            value = 150.0,
+            current_balance = 950.0,
+            timestamp = 246813579.0
+        )
+        repo.add_transaction(transaction1)
+        repo.add_transaction(transaction2)
+
+        response = get_history()
+        assert response["all_transactions"][0] == transaction1.to_dict()
+        assert response["all_transactions"][1] == transaction2.to_dict()
+        assert len(response["all_transactions"]) == 2
+
+
+    def test_post_deposit(self):
+        request = {"100": 1}
+        response = post_deposit(request=request)
+
+        assert response["current_balance"] == 1100.0
+        assert type(response["timestamp"]) == float
+        assert response["timestamp"] > 0
+
+
+    def test_post_deposit_multiple_notes(self):
+        request = {
+            "2": 50,
+            "5": 20,
+            "10": 10,
+            "20": 5,
+            "50": 2,
+            "100": 1,
+            "200": 1,
+        } #sum = 800
         
+        response = post_deposit(request=request)
+        assert response["current_balance"] == 1800.0
+
+    
+    def test_post_deposit_updates_balance(self):
+        request = {"50": 2}
+        post_deposit(request=request)
+
+        user = main_module.user_repo.get_first_user()
+        assert user.current_balance == 1100.0
+
+
+    def test_post_deposit_updates_history(self):
+        request = {"100": 1}  # 100.0
+        post_deposit(request=request)
+
+        history = get_history()
+        assert len(history["all_transactions"]) == 1
+        assert history["all_transactions"][0]["type"] == "DEPOSIT"
+        assert history["all_transactions"][0]["value"] == 100.0
+        assert history["all_transactions"][0]["current_balance"] == 1100.0
+
+
+    def test_post_deposit_suspicious(self):
+        request = {"200": 10}
+    
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+
+        assert err.value.status_code == 403
+
+
+    def test_post_deposit_empty_request(self):
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request={})
+
+        assert err.value.status_code == 400
+
+
+    def test_post_deposit_all_zeros(self):
+        request = {"2": 0} #main creates the other notes as 0
+
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+        assert err.value.status_code == 400
+        assert err.value.detail == "Sem notas selecionadas"
+
+
+    def test_post_deposit_invalid_note(self):
+        request = {"1": 1, "2": 2}
+        
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+        assert err.value.status_code == 400
+
+
+    def test_post_deposit_invalid_note_string(self):
+        request = {"2": 1, "string": 1}
+
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+        assert err.value.status_code == 400
+
+    
+    def test_post_deposit_negative_quantity(self):
+        request = {"2": -1}
+
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+        assert err.value.status_code == 400
+
+
+    def test_post_deposit_quantity_not_int(self):
+        request = {"2": 1.5}
+        
+        with pytest.raises(HTTPException) as err:
+            post_deposit(request=request)
+        assert err.value.status_code == 400
